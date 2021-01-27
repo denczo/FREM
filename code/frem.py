cd67cfd49d4e70a2d327d1598f41a7c931442680
Dennis <d.czogalla@gmx.de>
2021-01-27 23:01:22 +0100

    improved performance, overall smoother user experience

--- code/frem.py
+++ code/frem.py
@@ -1,11 +1,15 @@
+import os
+#os.environ["KIVY_NO_CONSOLELOG"] = "1"
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
-
+import pyaudio
 import numpy as np
 from kivy.clock import Clock
+
 
 from tools import *
 
@@ -13,7 +17,7 @@ from tools import *
 class FremApp(App):
 
     def build(self):
-        # Clock.schedule_interval(lambda dt: print("FPS: ", Clock.get_fps()), 1)
+        Clock.schedule_interval(lambda dt: print("FPS: ", Clock.get_fps()), 1)
         return MainGrid()
 
 
@@ -26,105 +30,121 @@ colors = [
 
 
 class CarrierWave:
-    def __init__(self, color, chunk_size=5000, waveform='Sine'):
-        self._waveform = waveform
+    def __init__(self, color, chunk_size, waveform='Sine'):
         self.chunk_size = chunk_size
-        self._graph_active = False
-        self._frequency = 1
-        self._x = np.linspace(0, 1, chunk_size)
-        self._y = 0
+        self.graph_active = False
+        self.waveform = waveform
+        self.frequency = 1
+        self.mod_wave = 0
+        self.equation = ''
+        self.color = color
+        self.x = np.linspace(0, 1, chunk_size)
+        self.y = 0
         self.formula = ''
+        self.symbol = 'f(x)'
         self.render_wf()
         self.render_equation()
-        self.color = color
 
-    @property
-    def waveform(self):
-        return self._waveform
+    def change_waveform(self, wf):
+        self.waveform = wf
+        self.render_wf()
+        self.render_equation()
 
-    @waveform.setter
-    def waveform(self, value):
-        self._waveform = value
+    def change_frequency(self, f):
+        self.frequency = f
+        self.render_wf()
 
-    @property
-    def graph_active(self):
-        return self._graph_active
+    def change_mod_wave(self, m):
+        self.mod_wave = m
+        self.render_wf()
 
-    @graph_active.setter
-    def graph_active(self, value):
-        self._graph_active = value
-
-    @property
-    def frequency(self):
-        return self._frequency
-
-    @frequency.setter
-    def frequency(self, value):
-        self._frequency = value
-
-    @property
-    def y(self):
-        return self._y
-
-    def render_wf(self, m=0, c=0):
-        #print(isinstance(self, ModulationWave))
+    def render_wf(self):
         # label, a = amplitude, f = frequency, x = samples, c = constant, m = modulation wave
-        self._y = current_trigon_wf(self._waveform, 0.5, self.frequency, self._x, c, m)
+        self.y = current_trigon_wf(self.waveform, 0.5, self.frequency, self.x, 0.5, self.mod_wave)
 
     def render_equation(self):
-        return current_equation(self._waveform, 'Trigonometric function')
+        self.equation = current_equation(self.waveform, 'Trigonometric function')
 
 
 class ModulationWave(CarrierWave):
-    def __init__(self, color):
-        super().__init__(color)
-        self._int_active = False
-        self._mod_index = 0.1
+    def __init__(self, color, chunk_size, waveform='Triangle'):
+        self.int_active = False
+        super().__init__(color, chunk_size, waveform)
+        self.mod_index = 0.1
         self.graph_active = True
 
-    @property
-    def int_active(self):
-        return self._int_active
+    def calculate_integral(self, value):
+        self.int_active = value
+        self.render_wf()
 
-    @int_active.setter
-    def int_active(self, value):
-        self._int_active = value
+    def discrete_integration(self):
+        self.y = running_sum(self.y, 0)
+        self.y = normalize(self.y)
 
-    @property
-    def mod_index(self):
-        return self._mod_index
-
-    @mod_index.setter
-    def mod_index(self, value):
-        self._mod_index = value / self.frequency
-
-    def apply_modulation(self, status):
-        if status:
-            return self.render_wf()
+    def render_wf(self):
+        if self.int_active:
+            self.symbol = 'F(x)'
+            # label, a = amplitude, f = frequency, x = samples, c = constant, m = modulation wave
+            self.y = current_trigon_wf(self.waveform, 0.5, self.frequency, self.x, 0, self.mod_wave)
+            self.discrete_integration()
         else:
-            return 0
+            self.symbol = 'f(x)'
+            # label, a = amplitude, f = frequency, x = samples, c = constant, m = modulation wave
+            self.y = current_trigon_wf(self.waveform, 0.5, self.frequency, self.x, 0.5, self.mod_wave)
+
+    def change_mod_index(self, mi):
+        self.mod_index = mi / self.frequency
+        self.render_wf()
+
+
+class AudioMaker:
+    def __init__(self, rate=44100, chunk_size=1024, gain=0.25):
+        self.rate = int(rate)
+        self.chunk_size = chunk_size
+        self.p = pyaudio.PyAudio()
+        self.stream = self.settings(1, self.rate, 1, self.chunk_size)
+        self.stream.start_stream()
+
+    def settings(self, channels, rate, output, chunk_size):
+        return self.p.open(format=pyaudio.paFloat32,
+                           channels=channels,
+                           rate=rate,
+                           output=output,
+                           frames_per_buffer=chunk_size)
+
+    def render_audio(self, length):
+
+        self.stream.write(self.chunk.astype(np.float32).tostring())
+        start = end
+        end += self.chunk_size
 
 
 class MainGrid(BoxLayout):
 
-    def __init__(self, wf_amp='Sine', wf_mod='Triangle', wf_car='Sine'):
+    def __init__(self):
         super(MainGrid, self).__init__()
-        self.mod_wave_1 = ModulationWave('#08F7FE')
-        self.mod_wave_2 = ModulationWave('#FE53BB')
-        self.carrier = CarrierWave('#00ff41')
+        chunk_size = 2000
+        self.mod_wave_1 = ModulationWave('#08F7FE', waveform='Sine', chunk_size=chunk_size)
+        self.mod_wave_2 = ModulationWave('#FE53BB', waveform='Triangle', chunk_size=chunk_size)
+        self.carrier = CarrierWave('#00ff41', chunk_size=chunk_size)
         self.waveforms = [self.mod_wave_1, self.mod_wave_2, self.carrier]
-
         self._current_tab = 'WF_M1'
 
         self.fig = plt.figure(facecolor='#212946')
         self.ax = self.fig.add_subplot(111)
+        self.ann = None
+        self.ann_color = None
         self.plot = FigureCanvasKivyAgg(self.fig)
+        self.plot_buffer = []
 
-        self.chunk_size = 5000
+        self.chunk_size = chunk_size
         self.ids.modulation.add_widget(self.plot)
         self.plot_x = np.linspace(0, 1, self.chunk_size)
         self.plot_y = np.zeros(self.chunk_size)
         self.formula = ''
+        self.old_formula = ''
+        self.lines = []
+        self.init_plot()
         self.update_plot()
 
     @property
@@ -137,75 +157,65 @@ class MainGrid(BoxLayout):
 
     def update_equation(self):
         if self._current_tab == 'WF_M1':
-            self.formula = self.mod_wave_1.render_equation()
+            self.formula = self.mod_wave_1.equation
+            self.ann_color = self.mod_wave_1.color
         elif self._current_tab == 'WF_M2':
-            self.formula = self.mod_wave_2.render_equation()
+            self.formula = self.mod_wave_2.equation
+            self.ann_color = self.mod_wave_2.color
         elif self._current_tab == 'WF_C':
-            self.formula = self.carrier.render_equation()
+            self.formula = self.carrier.equation
+            self.ann_color = self.carrier.color
+
+        if self.formula != self.old_formula:
+            if self.ann is not None:
+                self.ann.remove()
+            self.ann = self.ax.annotate(self.formula, xy=(0.5, -0.26), xycoords='axes fraction',
+                             fontsize=9, color=self.ann_color, bbox=dict(boxstyle="round", fc='black', ec="None", alpha=0.2),
+                             ha='center',
+                             va='center')
+            self.old_formula = self.formula
+            self.plot.draw()
+
+    def init_plot(self):
+        self.ax.tick_params(left=False, bottom=False, labelbottom=False, labelleft=False)
+        self.ax.set_facecolor('#212946')
+
+        plt.subplots_adjust(left=0.02, right=0.98, top=0.98, bottom=0.3)
+        plt.grid(color='grey', alpha=0.2)
 
     def update_plot(self):
 
-        plt.setp(self.ax.spines.values(), linewidth=3, color='grey', alpha=0.2)
-        self.ax.cla()
-
-        self.update_equation()
-
-        mod_wave = 0
+        #self.ax.cla()
+        for ln in self.lines:
+            ln.remove()
+        self.lines.clear()
+        wf_mod = 0
         for i in range(len(self.waveforms)):
-            wf = self.waveforms[i]
-            wf_y = wf.y
-            if isinstance(wf, ModulationWave):
-                if wf.int_active:
-                    # constant c need to be 0 for integral
-                    wf.render_wf(m=mod_wave, c=0)
-                    wf_y = wf.y
-                    wf_y = running_sum(wf_y, 0)
-                    wf_y = normalize(wf_y)
-                else:
-                    wf.render_wf(0, c=0.5)
-                    wf_y = wf.y
-                mod_wave = wf_y * wf.mod_index
-            else:
-                wf.render_wf(m=mod_wave, c=0.5)
+            wf_carrier = self.waveforms[i]
+            wf_carrier.change_mod_wave(wf_mod)
+            wf_y = wf_carrier.y
 
-            if wf.graph_active:
-                self.plot_graph(self.ax, self.plot_x, wf_y, wf.color)
+            if wf_carrier.graph_active:
+                self.update_equation()
+                self.plot_graph(self.ax, self.plot_x, wf_y, wf_carrier.color)
+                #self.ax.annotate(wf_carrier.symbol, xy=(0.02, 0.93), xycoords='axes fraction', fontsize=8, color='#F5D300', bbox=dict(boxstyle="round", fc='black', ec='None', alpha=0.4))
 
+            wf_mod = wf_carrier.y
 
-        mod_color = '#08F7FE'
-        amp_color = '#FE53BB'
-        car_color = '#00ff41'
-        symbol = 'f(x)'
         # if self.int_active:
         #     symbol = 'F(x)'
         #     self.formula = r'$\int$ ' + self.formula
         #     mod_color = '#F5D300'
 
-        # if self._car_active:
-        #     self.plot_graph(self.ax, self.plot_x, self.car_y, car_color)
-        #
-        # if self._mod_active:
-        #     self.plot_graph(self.ax, self.plot_x, self.mod_y, mod_color)
-        #     self.ax.annotate(symbol, xy=(0.02, 0.93), xycoords='axes fraction',
-        #                      fontsize=8, color=mod_color, bbox=dict(boxstyle="round", fc='black', ec='None', alpha=0.4))
-
-        self.ax.tick_params(left=False, bottom=False, labelbottom=False, labelleft=False)
-        self.ax.set_facecolor('#212946')
-
-        self.ax.annotate(self.formula, xy=(0.5, -0.26), xycoords='axes fraction',
-                         fontsize=9, color=mod_color, bbox=dict(boxstyle="round", fc='black', ec="None", alpha=0.2),
-                         ha='center',
-                         va='center')
-
-        plt.subplots_adjust(left=0.02, right=0.98, top=0.98, bottom=0.3)
-        plt.grid(color='grey', alpha=0.2)
         self.plot.draw()
 
-    @staticmethod
-    def plot_graph(ax, x, y, color):
-        ax.plot(x, y, linewidth=1.5, alpha=1, color=color)
+    def plot_graph(self, ax, x, y, color):
+
+        ln, = ax.plot(x, y, linewidth=1.5, alpha=1, color=color)
+        self.lines.append(ln)
         # glow effect
-        ax.plot(x, y, linewidth=6, alpha=0.1, color=color)
+        ln, = ax.plot(x, y, linewidth=6, alpha=0.1, color=color)
+        self.lines.append(ln)
 
 
 FremApp().run()
