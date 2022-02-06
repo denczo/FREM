e9b2df83428b2e9f5625b8e9b77636e0214b99f1
Dennis <d.czogalla@gmx.de>
2022-02-06 21:11:34 +0100

    ADD: info text about math

--- code/frem/main.py
+++ code/frem/main.py
@@ -165,7 +165,12 @@ class MainGrid(BoxLayout):
     #                                     "While analog synthesizer use physical components to generate the audio signal, digital synthesizer are doing this only with" \
     #                                     "mathematics and a digital computer chip. The vibrato effect can be realised mathematically by using frequency modulation." \
     #                                     "This App demonstrates it in an intuitive way and shows what else is possible with this concept."
-
+    # "To render a waveform with a " \
+    # "given frequency the amount of samples needs to be at least 2 times the frequency."\
+    # "If you would like to render a waveform with 20khz (20.000hz) which is the highest"\
+    # "frequency most people are able to hear, you would at least require 40.000 samples."\
+    # "Therefore in the music industry often a samplingrate of 44.1khz is used which " \
+    # "basically tells us that the audio is rendered with 44.100 samples per second." \
     @staticmethod
     def show_info():
         info = Info()  # Create a new instance of the P class
@@ -198,6 +203,38 @@ class MainGrid(BoxLayout):
                                     "To apply the modulating wave onto the carrier wave, the discrete integration of the " \
                                     "waveform needs to be calculated ([b] calculate F(x)[/b] ).\n\n[b]PLAY:[/b] plays " \
                                     "the carrier wave with it's applied modulation.\n"
+        info.ids.infoText_p5.text = "\n\n\n[b]Quick overview behind the maths[/b]" \
+                                    "\n\nThe maths behind frequency modulation works the same for all waveforms. Let's " \
+                                    "take as example the sine wave. As already mentioned we need a carrier wave and " \
+                                    "a modulation wave to do frequency modulation. Let's start with the formula " \
+                                    "for the [b]carrier wave[/b]:" \
+                                    "\n\n[b][size=20sp]"+LatexNodes2Text().latex_to_text(r'$a \sin(2\pi f x + m)$')+"[/size][/b]"\
+                                    "\n\n[i]a: amplitude (signal strength)\nf: frequency\nx: individual sample on x-axis" \
+                                    "\nm: modulating wave e.g. " \
+                                    "[b]"+LatexNodes2Text().latex_to_text(r'$a \sin(2\pi f x)$')+", 0 if not set[/b][/i]" \
+                                    "\n\nTo render a waveform with a given frequency the amount of samples needs to be " \
+                                    "at least 2 times the frequency. "\
+                                    "If you would like to render a waveform with 20khz (20.000hz) which is the highest "\
+                                    "frequency most people are able to hear, you would at least require 40.000 samples."\
+                                    "Therefore in the music industry often a sampling-rate of 44.1khz is used which " \
+                                    "basically tells us that the audio is rendered with 44.100 samples per second." \
+                                    "\n\n\nThe modulating wave is simply another carrier wave. Let's have a look how " \
+                                    "this [b]modulating wave[/b] can be applied on the carrier wave:\n\n" \
+                                    "[size=20sp]"+LatexNodes2Text().latex_to_text(r'$a[sub]c[/sub] \sin(2\pi f[sub]c[/sub] x[sub]c[/sub] + [b]\beta a[sub]m[/sub] \int\ sin(2\pi f[sub]m[/sub] x[sub]m[/sub])dx[/b])$')+"[/size]"\
+                                    "\n\n"+LatexNodes2Text().latex_to_text(r'$[i]\beta$')+": modulation index" \
+                                    " ("+LatexNodes2Text().latex_to_text(r'$f[sub]\Delta[/sub]/f[sub]m[/sub]$')+") \n" \
+                                    "a[sub]m[/sub]: amplitude modulation wave\nf[sub]m[/sub]: frequency modulation wave" \
+                                    "\nx[sub]m[/sub]: individual sample of modulation wave on x-axis\n\n\n" \
+                                    "To calculate the integral you first will need to calculate all the samples for " \
+                                    "the modulation wave. After that you apply the [b]running sum[/b] on each of " \
+                                    "those samples and that's it:\n\n[b][size=20sp]x[sub]n[/sub] = x[sub]n[/sub] - x[sub]" \
+                                    "n-1[/sub][/size][/b]\n\nx[sub]n[/sub]: individual sample\nfor n = 0:x[sub]n-1[/sub] = 0" \
+                                    "\n\nIn the last step you will need to [b]normalize[/b] the samples of your modulating wave." \
+                                    "After that you can simply add each sample of the modulating wave to each sample of" \
+                                    "the carrier wave (just like in the formula). \n\n[size=20sp][b]x[sub]n[/sub] = x[sub]n[/sub]" \
+                                    " - min(x) / max(x) - min(x)[/b][/size]\n\nx[sub]n[/sub]: individual sample\nx: all samples"
+
+
 
     def init_max_min(self):
         for wf in self.wf_labels:
